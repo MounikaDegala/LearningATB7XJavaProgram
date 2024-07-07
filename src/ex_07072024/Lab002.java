245e98ac0f4b8cdb96b269074ea471ebaa0dca2f
Mounika Degala <MounikaD@hexaware.com>
2024-07-07 18:50:06 +0530

    All new files are uploaded

--- src/ex_07072024/Lab002.java
+++ src/ex_07072024/Lab002.java
@@ -1,7 +1,17 @@
 package ex_07072024;
 
 public class Lab002 {
-    public static void main(String[] args) {
-        System.out.println(2*2);
+
+    public static void main(String[] mounika) {
+        int a = 10;
+        int b=2;
+        //multiplication
+        System.out.println("multiplication of a&b="+(a*b));
+        //subtraction
+        System.out.println("subtraction of a&b="+(a-b));
+        //addition
+        System.out.println("addition of a&b="+(a+b));
+        //division
+        System.out.println("division of a&b="+(a/b));
     }
 }
